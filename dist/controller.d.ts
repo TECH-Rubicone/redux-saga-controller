fabb8da2fddc54e7163599f451cbc314ff388e02
Ivan Chaban <ichabanivan@gmail.com>
2020-11-18 20:49:37 +0200

    environment/BUILD-Add-build-files
    - Add build files

---
+++ dist/controller.d.ts
@@ -0,0 +1,19 @@
+import { ActionCreator, AnyAction } from 'redux';
+export declare type DefaultActions = 'updateCtrl' | 'clearCtrl';
+export declare class Controller<T extends string, I> {
+    initial: I;
+    name: string;
+    TYPE: Record<T, string>;
+    selector: (name: string) => any;
+    subscriber: any;
+    action: Record<T | "updateCtrl" | "clearCtrl", ActionCreator<AnyAction>>;
+    private _channel;
+    constructor({ types, prefix, initial, subscriber }: {
+        initial: I;
+        prefix?: string;
+        subscriber: any;
+        types: Array<T>;
+    });
+    set channel(channel: any | null);
+    get channel(): any | null;
+}
